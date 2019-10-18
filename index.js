ef7c8e4ce78808ac6a87cb62c48406f7e0ea56af
Justin <justincase18@msn.com>
2019-10-18 15:48:07 -0800

    take 1

--- index.js
+++ index.js
@@ -1,41 +1,41 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser')
-const knex = require('knex')
-const pg = require('pg')
+// const bodyParser = require('body-parser')
+// const knex = require('knex')
+// const pg = require('pg')
 
-const db = knex({
-    client: 'pg',
-    connection: {
-        connectionString: process.env.DATABASE_URL,
-        ssl: true,
-    }    
-})
+// const db = knex({
+//     client: 'pg',
+//     connection: {
+//         connectionString: process.env.DATABASE_URL,
+//         ssl: true,
+//     }    
+// })
 
 
 const app = express();
 
-// Serve static files from the React app
-//app.use(express.static(path.join(__dirname, 'client/build')));
+//Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 // Put all API endpoints under '/api' this is an exampe
-app.get('/api/routelist', (req, res) => {
+// app.get('/api/routelist', (req, res) => {
 
-    db.select('*').from('routes')
-    .then(data => {
-        res.json(data)
-    })  
+//     db.select('*').from('routes')
+//     .then(data => {
+//         res.json(data)
+//     })  
 
-});
+// });
 
-app.get('/', (req, res) => {
-    return("hello world")
-})
+// app.get('/', (req, res) => {
+//     res.send("hello world")
+// })
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-// app.get('*', (req, res) => {
-//   res.sendFile(path.join(__dirname+'/client/build/index.html'));
-// });
-
-app.listen(process.env.PORT);
+ app.get('*', (req, res) => {
+   res.sendFile(path.join(__dirname+'/client/build/index.html'));
+ });
+app.listen(5000)
+//app.listen(process.env.PORT);
