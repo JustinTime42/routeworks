3be11412339f2f7e52e55fe4f1143cc6ed72b9a4
Justin <justincase18@msn.com>
2019-10-31 11:14:23 -0800

    added names to addresslist and some code for dnd

--- client/src/components/AddressList.js
+++ client/src/components/AddressList.js
@@ -2,12 +2,16 @@ import React, { Component } from "react"
 import Can from "../components/Can"
 import { AuthConsumer } from "../authContext"
 import { connect } from "react-redux"
+import Card from "react-bootstrap/Card"
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
 
 import { requestAllAddresses } from "../actions"
 
 const mapStateToProps = state => {
     return {
         addresses: state.requestAllAddresses.addresses,
+        // add selectedAddresses
         isPending: state.requestAllAddresses.isPending,
         error: state.requestAllAddresses.error    
     }
@@ -21,14 +25,55 @@ const mapDispatchToProps = (dispatch) => {
 
 const renderAddress = (address) => {
     return(
-        <div>
-            <p>{address.address}</p>
-        </div>
+        <Card>
+            <Card.Body>
+                <Card.Text>{address.address}</Card.Text>
+                <Card.Text>{address.cust_name}</Card.Text>
+            </Card.Body>
+        </Card>
     )
 }
 
+/**
+ * Moves an item from one list to another list.
+ */
+const move = (source, destination, droppableSource, droppableDestination) => {
+    const sourceClone = Array.from(source);
+    const destClone = Array.from(destination);
+    const [removed] = sourceClone.splice(droppableSource.index, 1);
+
+    destClone.splice(droppableDestination.index, 0, removed);
+
+    const result = {};
+    result[droppableSource.droppableId] = sourceClone;
+    result[droppableDestination.droppableId] = destClone;
+
+    return result;
+};
+
+const grid = 8;
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+    // some basic styles to make the items look a bit nicer
+    userSelect: 'none',
+    padding: grid * 2,
+    margin: `0 0 ${grid}px 0`,
+
+    // change background colour if dragging
+    background: isDragging ? 'lightgreen' : 'grey',
+
+    // styles we need to apply on draggables
+    ...draggableStyle
+});
+
+const getListStyle = isDraggingOver => ({
+    background: isDraggingOver ? 'lightblue' : 'lightgrey',
+    padding: grid,
+    width: 250
+});
 
 class AddressList extends Component {
+
 
     componentDidMount() {
         this.props.onGetAllAddresses()
