d66ad2eefcbfd600106d20d36f26e7c8b615c6b4
Justin <justincase18@msn.com>
2019-11-11 16:46:33 -0900

    test

--- index.js
+++ index.js
@@ -29,15 +29,15 @@ app.get('/api/routelist', (req, res) => {
     })  
 });
 
-app.post('/api/addroute', (req, res) => {
-    res.json(req.body.route_name)
-    // db('routes')
-    // .returning('route_name')
-    // .insert({route_name: req.body.route_name})
-    // .then(routeName => {
-    //     res.json(routeName)
-    // }) 
-})
+// app.post('/api/addroute', (req, res) => {
+//     res.json(req.body.route_name)
+//     // db('routes')
+//     // .returning('route_name')
+//     // .insert({route_name: req.body.route_name})
+//     // .then(routeName => {
+//     //     res.json(routeName)
+//     // }) 
+// })
 
 app.post('api/saveroute', (req, res) => {
     res.json(req.body.addresses)
