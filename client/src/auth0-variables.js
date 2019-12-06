1ba934fcb96a64c023da38b7d47cec0c3ebc22dc
Justin <justincase18@msn.com>
2019-12-05 22:48:57 -0900

    progress on newProperty modal

--- client/src/auth0-variables.js
+++ client/src/auth0-variables.js
@@ -2,6 +2,6 @@ export const AUTH_CONFIG = {
     domain: "justintime42.auth0.com",
     roleUrl: "https://snowline-route-manager.herokuapp.com/role",
     clientId: "4a5CpQ0kBBRvRIExud9aiDsxJQNiWsOe",
-    callbackUrl: "https://snowline-route-manager.herokuapp.com/callback"
-    //callbackUrl: "http://localhost:3000/callback"
-  };
+    //callbackUrl: "https://snowline-route-manager.herokuapp.com/callback"
+    callbackUrl: "http://localhost:3000/callback"
+  }
--- client/src/components/NewProperty.js
+++ client/src/components/NewProperty.js
@@ -1,30 +1,84 @@
 import React, { Component } from 'react'
-import { Button, Modal } from 'react-bootstrap'
+import { Button, Modal, Form, Row, Col } from 'react-bootstrap'
 
 class NewProperty extends Component {
-    constructor(){
-        super()
+    constructor(props){
+        super(props)
         this.state = {
-            show: false
+            show: false,
+            address: '',
+            cust_name: '',
+            cust_phone: '',
+            surface_type: '',
+            is_new: false,
+            notes: ''
         }
+    }
+
+    onSubmit = () => {
+        console.log(this.state)
+   
+    }
+
+    onChange = (event) => {
+        console.log(event.target.value)
+        const name = event.target.name
+        const value = event.target.value
+        this.setState({
+            [name]: value
+        })
     }
 
     render() {
         return (
             <Modal show={this.props.show} onHide={this.handleClose}>
-                    <Modal.Header>[Placeholder]</Modal.Header>
+                    <Modal.Header>New Property</Modal.Header>
                     <Modal.Body>
-                        <form>
-                            <input onChange={this.onSetRouteName} type="text" name="routeName" placeholder="[placeholder]"></input>
-                        </form>
+                        <Form>
+                            <Form.Group as={Row}>
+                                <Form.Label column sm={2}>Address</Form.Label>
+                                <Col sm={10}>
+                                    <Form.Control name="address" type="text" placeholder="Address" onChange={this.onChange}/>
+                                </Col>
+                            </Form.Group>
+                            <Form.Group as={Row}>
+                                <Form.Label column sm={2}>Name</Form.Label>
+                                <Col sm={10}>
+                                    <Form.Control name="cust_name" type="text" placeholder="Name" onChange={this.onChange}/>
+                                </Col>
+                            </Form.Group>
+                            <Form.Group as={Row}>
+                                <Form.Label column sm={2}>Phone</Form.Label>
+                                <Col sm={10}>
+                                    <Form.Control name="cust_phone" type="text" placeholder="Phone" onChange={this.onChange}/>
+                                </Col>
+                            </Form.Group>
+                            <Form.Group as={Row}>
+                                <Form.Label>Surface Type</Form.Label>
+                                <Form.Control as="select" onChange={this.onChange}>
+                                    <option value="paved">Paved</option>
+                                    <option value="gravel">Gravel</option>
+                                    <option value="partial">Partial</option>
+                                </Form.Control>
+                            </Form.Group>
+                            
+                            
+                        <Form.Control type="textarea" rows="3" />
+                        <Button variant="primary" onClick={this.onSubmit}>Save Changes</Button>
+                        </Form>        
+                        {/* address: req.body.address,
+                        cust_name: req.body.cust_name,
+                        cust_phone: req.body.cust_phone,
+                        surface_type: req.body.surface_type,
+                        is_new: req.body.is_new,
+                        notes: req.body.notes */}
                     </Modal.Body>
                     <Modal.Footer>
                         <Button variant="secondary" onClick={this.props.close}>
                         Close
                         </Button>
-                        <Button variant="primary" onClick={this.handleSave}>
-                        Save Changes
-                        </Button>
+                        
+                        
                     </Modal.Footer>
                 </Modal>
         )
