f3653e7a84a6b43a2287a614472c27f1538e49b1
Justin <justincase18@msn.com>
2019-12-17 13:15:35 -0900

    update packages

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,5 @@
 // src/index.js
-//import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
