b53a995867464ec546cde387345b4ce2f2b799aa
Justin <justincase18@msn.com>
2020-02-06 14:58:18 -0900

    test

--- client/src/auth0-variables.js
+++ client/src/auth0-variables.js
@@ -2,7 +2,7 @@ export const AUTH_CONFIG = {
   domain: "justintime42.auth0.com",
   roleUrl: "https://snowline-route-manager.herokuapp.com/role",
   clientId: "4a5CpQ0kBBRvRIExud9aiDsxJQNiWsOe",
-  callbackUrl: process.env.callbackUrl,
-  // callbackUrl: "https://snowline-route-dev.herokuap.com/callback",
+  //callbackUrl: process.env.callbackUrl,
+  callbackUrl: "https://snowline-route-dev.herokuapp.com/callback",
   // callbackUrl: "http://localhost:3000/callback"
 }
