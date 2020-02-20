ddc3076600136254366430a0b06e9adb039c429d
Justin <justincase18@msn.com>
2020-02-19 22:19:43 -0900

    test

--- client/src/components/PropertyCard.js
+++ client/src/components/PropertyCard.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Button } from 'react-bootstrap'
-import { shallow, mount, render } from 'enzyme'
+//import { shallow, mount, render } from 'enzyme'
 
 const PropertyCard = (props) => {
 
