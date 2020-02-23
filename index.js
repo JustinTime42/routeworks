eda5611a265206ff9391a18218919fb3eb8de27f
Justin <justincase18@msn.com>
2020-02-23 13:30:09 -0900

    increased file size limit for post operations

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ const db = knex({
 
 const app = express();
 app.use(cors())
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: false }));
+app.use(bodyParser.json({limit: '50mb'}));
 //Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
