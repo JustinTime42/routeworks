5a7b33ab4de88fa9997c9ce1e69d216b8a3ab223
Justin <justincase18@msn.com>
2020-02-22 23:25:56 -0900

    reverting

--- index.js
+++ index.js
@@ -225,15 +225,23 @@ app.get('/api/getroute/:routeName', (req, res) => {
     // the column is latest record for that property from service_log table
 
     // for each property, take the most recent record from service_log
-    //WHERE key===key ORDER BY timestamp DESC LIMIT 1;
+    //WHERE key===key ORDER BY date DESC LIMIT 1;
 
+    // const { routeName } = req.params
+    // db.where('properties.route_name', routeName)
+    // .select('*') 
+    // .from('properties')
+    // .leftJoin('service_log', () => {
+    //     on('properties.key', 'service_log.property_key').orderBy('service_log.timestamp', 'desc').limit(1)
+    // })
+    // .orderBy('route_position')
+    // .then(data => {
+    //     res.json(data)
+    // })
     const { routeName } = req.params
     db.where('properties.route_name', routeName)
     .select('*') 
     .from('properties')
-    .leftJoin('service_log', () => {
-        on('properties.key', 'service_log.property_key').orderBy('service_log.timestamp', 'desc').limit(1)
-    })
     .orderBy('route_position')
     .then(data => {
         res.json(data)
