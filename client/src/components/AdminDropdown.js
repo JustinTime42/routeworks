5ecce30c8a2163d3cbe05da3e884b6326fdfe77c
Justin <justincase18@msn.com>
2020-02-25 17:31:43 -0900

    process.env callbackurl

--- client/src/components/AdminDropdown.js
+++ client/src/components/AdminDropdown.js
@@ -65,7 +65,7 @@ class EditRouteButton extends Component {
         .then(logs => {
             logs.forEach((item => {
                
-                item.timestamp = new Date(item.timestamp).toLocaleString("en-US", {timeZone: "America/Anchorage"})
+                item.timestamp = new Date(item.timestamp).toLocaleString("en-CA", {timeZone: "America/Anchorage"})
             })) 
             this.setState({logs: logs, showDownload: true})
         })
