6eea061245e85c8e4c9001aac8a68f5bd7b9e423
Justin <justincase18@msn.com>
2020-06-08 15:08:53 -0800

    fixed callbackurl

--- client/src/auth0-variables.js
+++ client/src/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
   domain: "justintime42.auth0.com",
-  roleUrl: "https://snowline-route-manager.herokuapp.com/role",
+  roleUrl: "https://route-manager-app.herokuapp.com/role",
   clientId: "4a5CpQ0kBBRvRIExud9aiDsxJQNiWsOe",
   // callbackUrl: process.env.REACT_APP_CALLBACK_URL,
   // callbackUrl: "https://snowline-route-manager.herokuapp.com/callback",
