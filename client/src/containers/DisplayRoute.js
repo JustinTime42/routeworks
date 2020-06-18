54b989994d5867e7e5111d9f509e60f133c67a17
Justin <justincase18@msn.com>
2020-06-18 15:00:53 -0800

    fixed typo in displayroute changeactiveproperty

--- client/src/containers/DisplayRoute.js
+++ client/src/containers/DisplayRoute.js
@@ -50,14 +50,12 @@ class DisplayRoute extends Component {
             let currentPosition = this.parseRouteData(property, this.props.activeRoute, 'route_position')
             let nextPosition = (direction === 'next') ? currentPosition++ : currentPosition--
             this.props.onSetActiveProperty(this.props.routeProperties.find(customer => 
-                this.parseRouteData(customer, this.props.activeRoute, 'route_position').route_position = nextPosition))    
+                this.parseRouteData(customer, this.props.activeRoute, 'route_position').route_position === nextPosition))    
         } else {
             this.props.onSetActiveProperty(property)
         }
         
-        //this is ridicules. I went to a bunch of work to protect against non-consecutive numbers, but those can't happen and shouldn't happen. 
-        // I fixed the issue with that. So... now I need to refactor this code to simply go to the next or previous item on the route 
-        // simple increment or decrement. 
+
         // const previous = (i) => {
         //     return this.props.routeProperties.slice().reverse().find(item => {
         //         return (
