79a97b5280bc80d7dc98d61289651179babc6c34
Justin <justincase18@msn.com>
2020-06-19 14:31:29 -0800

    cleaned up

--- client/src/auth0-variables.js
+++ client/src/auth0-variables.js
@@ -1,12 +1,11 @@
 export const AUTH_CONFIG = {
   domain: "justintime42.auth0.com",
-  roleUrl: process.env.REACT_APP_ROLE_URL,
-  clientId: process.env.REACT_APP_CLIENTID,
-  // callbackUrl: process.env.REACT_APP_CALLBACK_URL,
-  callbackUrl: process.env.REACT_APP_CALLBACK_URL, // "https://snowline-route-manager.herokuapp.com/callback",
-  // callbackUrl: "https://route-manager-app.herokuapp.com/callback",
-    // callbackUrl: "http://localhost:3000/callback",
-  //Now, I seem to need to set this env variable in heroku before git push heroku. 
-  //then, it takes that configuration with it to productions... *sigh*
 
+  // roleUrl:  "https://route-manager-app.herokuapp.com/role", 
+  // callbackUrl: "http://localhost:3000/callback",
+  // clientId:"31gTnvhuk71pvHNuzTi4wDV33notQ39V", 
+
+  roleUrl: process.env.REACT_APP_ROLE_URL,
+  callbackUrl: process.env.REACT_APP_CALLBACK_URL,
+  clientId:process.env.REACT_APP_CLIENTID,
 }
