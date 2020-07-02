a4a18c1ecb4a7dd9d1545dc6c80bbd6f058308e3
Justin <justincase18@msn.com>
2020-07-02 10:15:27 -0800

    added billing and email info for customers

--- client/src/auth0-variables.js
+++ client/src/auth0-variables.js
@@ -1,11 +1,11 @@
 export const AUTH_CONFIG = {
   domain: "justintime42.auth0.com",
 
-  roleUrl:  "https://route-manager-app.herokuapp.com/role", 
-  callbackUrl: "http://localhost:3000/callback",
-  clientId:"31gTnvhuk71pvHNuzTi4wDV33notQ39V", 
+  // roleUrl:  "https://route-manager-app.herokuapp.com/role", 
+  // callbackUrl: "http://localhost:3000/callback",
+  // clientId:"31gTnvhuk71pvHNuzTi4wDV33notQ39V", 
 
-  // roleUrl: process.env.REACT_APP_ROLE_URL,
-  // callbackUrl: process.env.REACT_APP_CALLBACK_URL,
-  // clientId:process.env.REACT_APP_CLIENTID,
+  roleUrl: process.env.REACT_APP_ROLE_URL,
+  callbackUrl: process.env.REACT_APP_CALLBACK_URL,
+  clientId:process.env.REACT_APP_CLIENTID,
 }
