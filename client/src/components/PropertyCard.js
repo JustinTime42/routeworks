7b1fcb937fb1093d0f92b095831ba42c31eca0f1
JustinTime42 <justincase18@msn.com>
2020-11-03 13:45:48 -0900

    fixed visual indicator for seasonal customers

--- client/src/components/PropertyCard.js
+++ client/src/components/PropertyCard.js
@@ -36,8 +36,7 @@ const PropertyCard = (props) => {
     }
 
     const seasonalStyle = {
-        borderLeft: props.address ? props.address.contract_type === 'Seasonal' || props.address.contract_type === 'monthly' ? "10px solid #375A7F" : "none" : "none"
-   
+        border: props.address ? props.address.contract_type === 'Seasonal' || props.address.contract_type === 'monthly' ? "5px solid #375A7F" : "none" : "none"   
     }
 
     const editStyle = {
