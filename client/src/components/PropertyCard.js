9e46542b36d14108e993048578e99f38cb47d3f0
JustinTime42 <justincase18@msn.com>
2020-11-03 14:55:11 -0900

    testing seasonal styles

--- client/src/components/PropertyCard.js
+++ client/src/components/PropertyCard.js
@@ -36,7 +36,8 @@ const PropertyCard = (props) => {
     }
 
     const seasonalStyle = {
-        border: props.address ? props.address.contract_type === 'Seasonal' || props.address.contract_type === 'monthly' ? "5px solid #a9c1da" : "none" : "none"   
+        border: props.address ? props.address.contract_type === 'Seasonal' || props.address.contract_type === 'monthly' ? "5px solid #375A7F" : "none" : "none",   
+        boxShadow: props.address ? props.address.contract_type === 'Seasonal' || props.address.contract_type === 'monthly' ? "5px, 10px, 15px, rgba(255,200,0,0.9)"
     }
 
     const editStyle = {
