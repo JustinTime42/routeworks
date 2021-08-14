ee63cc3aacc6582d7ba9b6049393f9618f12ffe5
JustinTime42 <justincase18@msn.com>
2021-08-13 16:13:17 -0800

    fixed timer to remain accurate

--- client/src/components/customer_panels/TimeTracker.js
+++ client/src/components/customer_panels/TimeTracker.js
@@ -2,20 +2,22 @@ import React, { useEffect, useState } from 'react'
 import { Tabs, Tab, Button, Modal, Form, Row, Col, Alert } from 'react-bootstrap'
 import { useDispatch, useSelector } from 'react-redux'
 
-let interval = 'test'
+let interval
 
-const TimeTracker = (props) => {
+const TimeTracker = props => {
     const [timeElapsed, setTimeElapsed] = useState(0)
     const [isRunning, setIsRunning] = useState(false)
+    const [startTime, setStartTime] = useState(0)
     
     useEffect(() => {        
-        if(isRunning && (timeElapsed === 0)){
-            interval = setInterval(() => setTimeElapsed(timeElapsed => timeElapsed + 1000), 1000)
-        } else if (!isRunning) {
-            clearInterval(interval) 
-            setTimeElapsed(0)                  
+        if(isRunning && (timeElapsed === 0)){            
+            interval = setInterval(() => setTimeElapsed(Date.now() - startTime), 500)
+        } else if (isRunning) {
+            setTimeElapsed(Date.now() - startTime)
         }
     },[isRunning])
+
+
 
     const DisplayTime = () => {
         let hours = Math.floor(timeElapsed / 3600000).toString().padStart(2,'0')
@@ -24,21 +26,23 @@ const TimeTracker = (props) => {
         return <Form.Label>{hours}:{minutes}:{seconds}</Form.Label>
     }
 
+    const onStartPress = () => {
+            setStartTime(Date.now())
+            setIsRunning(true)
+    }
+
     const onStopPress = () => {
+        clearInterval(interval) 
+        setTimeElapsed(0)
         setIsRunning(false)
         props.enableButtons()
-
-
-        //enable buttons
         //save time to server
-        
-
     }
 
     return (
         <Row className='buttonRowStyle' style={{ width:"70%", marginRight:'auto', marginLeft:'auto'}}>
         <h4>Log Time</h4>
-        <Button size='lg' onClick={()=>setIsRunning(true)}>Start</Button>
+        <Button size='lg' onClick={onStartPress}>Start</Button>
         <DisplayTime />
         <Button size='lg' onClick={onStopPress}>Stop</Button>                                    
     </Row>
