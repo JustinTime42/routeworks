a8cf4d13ca28b15c43127aebfb224e0803c0a727
JustinTime42 <justincase18@msn.com>
2022-10-07 12:27:31 -0800

    enter key now submits login form

--- src/auth/UserLogin.js
+++ src/auth/UserLogin.js
@@ -6,34 +6,43 @@ export const UserLogin = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')  
 
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    logInWithEmailAndPassword(username, password)
+  }
+
   return (
     <Card className="text-center" style={{ width: '18rem', marginTop: '2em', marginLeft: 'auto', marginRight: 'auto' }}>
       <Card.Header>Login</Card.Header>
       <Card.Body>
-        <Card.Text>
-          <Form.Control
-            value={username}
-            onChange={(event) => setUsername(event.target.value)}
-            placeholder="Username"
+        <Form>
+          <Card.Text>
+            <Form.Control
+              value={username}
+              onChange={(event) => setUsername(event.target.value)}
+              placeholder="Username"
+              size="large"
+              className="form_input"
+            />
+            <Form.Control
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+              placeholder="Password"
+              size="large"
+              type="password"
+              className="form_input"
+            />
+          </Card.Text>
+          <Button
+            onClick={handleSubmit}
+            variant="primary"
             size="large"
-            className="form_input"
-          />
-          <Form.Control
-            value={password}
-            onChange={(event) => setPassword(event.target.value)}
-            placeholder="Password"
-            size="large"
-            type="password"
-            className="form_input"
-          />
-        </Card.Text>
-        <Button
-          onClick={() => logInWithEmailAndPassword(username, password)}
-          variant="primary"
-          size="large"
-        >
-          Log In
-        </Button>
+            type="submit"
+          >
+            Log In
+          </Button>
+        </Form>
+        
       </Card.Body>
     </Card>    
   )
